09b25a6e176d8810d8f155e3a8cb80db305b6d22
HSH <han41562@gmail.com>
2023-11-17 10:57:53 +0900

    refactor: refactoring jwt filter to exception handling

--- src/main/java/com/example/HardBoard/config/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/HardBoard/config/filter/JwtAuthenticationFilter.java
@@ -40,37 +40,23 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		// TODO Test
 
 		String header = request.getHeader(JwtProperties.HEADER_STRING);
-		if (header == null || !header.startsWith(JwtProperties.TOKEN_PREFIX)) {
-			try{
-				chain.doFilter(request, response);
-			} catch (AccessDeniedException e){
-				log.debug("access failed handler receive access denied exception");
-				ObjectMapper mapper = new ObjectMapper();
-				String result = mapper.writeValueAsString(ApiResponse.of(HttpStatus.FORBIDDEN, "Authorization failed"));
-				response.getWriter().write(result);
-				response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-			} catch (Exception e){
-				log.debug(e.getMessage());
-
-			}
-
+		if (header == null) {
+			chain.doFilter(request, response);
 			return;
+		} else if(!header.startsWith(JwtProperties.TOKEN_PREFIX)){
+			throw new IllegalArgumentException("token prefix is wrong");
 		}
 		String token = request.getHeader(JwtProperties.HEADER_STRING)
 				.replace(JwtProperties.TOKEN_PREFIX, "");
 
-		// 토큰 검증 (이게 인증이기 때문에 AuthenticationManager도 필요 없음)
-		// 내가 SecurityContext에 집적접근해서 세션을 만들때 자동으로 UserDetailsService에 있는
-		// loadByUsername이 호출됨.
 		String email = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET)).build().verify(token)
 				.getClaim("email").asString();
 		if (StringUtils.hasText(email) == false) {
-			log.debug("access denied");
-			throw new AccessDeniedException("유저를 찾을 수 없습니다");
+			throw new IllegalArgumentException("Email claim is null");
 		}
 
 		User user = userRepository.findByEmail(email)
-				.orElseThrow(() -> new AccessDeniedException("유저를 찾을 수 없습니다"));
+				.orElseThrow(() -> new IllegalArgumentException("Email is wrong"));
 
 		SecurityContext context = SecurityContextHolder.createEmptyContext();
 		PrincipalDetails principalDetails = new PrincipalDetails(user);
--- src/main/java/com/example/HardBoard/config/filter/JwtExceptionHandlingFilter.java
+++ src/main/java/com/example/HardBoard/config/filter/JwtExceptionHandlingFilter.java
@@ -5,6 +5,7 @@ import com.example.HardBoard.api.ApiResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -16,19 +17,28 @@ import java.time.LocalDateTime;
 
 @Slf4j
 public class JwtExceptionHandlingFilter extends OncePerRequestFilter {
+    ObjectMapper mapper = new ObjectMapper();
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try{
             filterChain.doFilter(request,response);
         } catch (IllegalArgumentException |
-                 SignatureVerificationException |
-                 TokenExpiredException |
+                SignatureVerificationException |
                  JWTDecodeException |
                  MissingClaimException |
                  IncorrectClaimException ex){
-            ObjectMapper mapper = new ObjectMapper();
-            log.debug("Authentication failed");
-            String result = mapper.writeValueAsString(ApiResponse.of(HttpStatus.FORBIDDEN, ex.getMessage(), null));
+            String result = mapper.writeValueAsString(ApiResponse.of(HttpStatus.BAD_REQUEST, ex.getMessage(), null));
+
+            response.getWriter().write(result);
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        } catch (TokenExpiredException ex){
+            String result = mapper.writeValueAsString(ApiResponse.of(HttpStatus.BAD_REQUEST, "The Token has expired", null));
+
+            response.getWriter().write(result);
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        } catch (AccessDeniedException ex){
+            String result = mapper.writeValueAsString(ApiResponse.of(HttpStatus.UNAUTHORIZED, ex.getMessage(), null));
 
             response.getWriter().write(result);
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
